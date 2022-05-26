ab78152970a75d2f226bc323aadb76b64a884da7
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-05-26 21:19:56 +0900

    LinkSection 추가

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -2,15 +2,34 @@ import React from "react";
 import styled from "styled-components";
 import LyricsSection from "./LyricsSection/LyricsSection";
 
+const LINKS = [
+  {
+    id: 1,
+    title: "INSTAGRAM",
+    link: "https://www.instagram.com/bsm.seongnam/",
+  },
+  {
+    id: 2,
+    title: "YOUTUBE",
+    link: "https://www.youtube.com/channel/UCMQqz5LLzwKfWoeYw9bA2sQ/",
+  },
+  {
+    id: 3,
+    title: "PODCAST",
+    link: "https://www.podbbang.com/channels/1776176/",
+  },
+];
+
 const Landing = () => {
   return (
     <StyledLanding>
       <LyricsSection />
+
       <IntroSection>
         <SectionTitle>
           <h2>소개</h2>
         </SectionTitle>
-        <SectionParagraph>
+        <SectionDescription>
           <p>
             서있는 사람들은 각각 다른 그룹에 있던 사람들이 뜻을 모아 2020년에
             만든 성남 FC 서포터즈 소모임입니다. 우리는 '우리의 열정엔 쉼이
@@ -36,8 +55,29 @@ const Landing = () => {
             </IntroLink>
             에 DM을 보내주시면 답변드리겠습니다.
           </p>
-        </SectionParagraph>
+        </SectionDescription>
       </IntroSection>
+
+      <LinkSection>
+        <SectionTitle>
+          <h2>링크</h2>
+        </SectionTitle>
+        <SectionDescription>
+          <LinkList>
+            {LINKS.map(({ id, title, link }) => (
+              <li key={id}>
+                <LinkItem
+                  href={link}
+                  target="_blank"
+                  rel="noopenner noreferrer"
+                >
+                  {title}
+                </LinkItem>
+              </li>
+            ))}
+          </LinkList>
+        </SectionDescription>
+      </LinkSection>
     </StyledLanding>
   );
 };
@@ -60,7 +100,7 @@ const SectionTitle = styled.div`
   line-height: 1.5;
 `;
 
-const SectionParagraph = styled(SectionTitle)`
+const SectionDescription = styled(SectionTitle)`
   width: 80%;
   text-indent: 2em;
 `;
@@ -70,4 +110,19 @@ const IntroLink = styled.a`
   border-bottom: 1px solid ${({ theme }) => theme.border};
   font-weight: 400;
   text-indent: 0;
+`;
+
+const LinkSection = styled(IntroSection)``;
+
+const LinkList = styled.ul`
+  text-indent: 0;
+
+  li {
+    margin-bottom: 12px;
+  }
+`;
+
+const LinkItem = styled(IntroLink)`
+  font-weight: 300;
+  border-width: 2px;
 `;
